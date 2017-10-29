06310997b9e625d8f54d9b56fa2a2bdf3c38ecbb
dude <vovan_trc@mail.ru>
2017-10-29 18:37:26 +0300

    #FirstStep

---
+++ src/main/java/ru/dude/simplepeg/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.dude.simplepeg;
+
+/**
+ * Created by dude on 29.10.2017.
+ */
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("HelloWorld");
+    }
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,5 @@
+/**
+ * Created by dude on 29.10.2017.
+ */
+public class Tests {
+}
